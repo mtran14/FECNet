32cc08b923882054768b4318ed45f278c8f8eb70
nhatminh886688 <mtran14@u.rochester.edu>
2021-07-20 21:38:11 -0400

    asd

--- extract_features.py
+++ extract_features.py
@@ -11,8 +11,6 @@ from multiprocessing import Process, Manager
 import random
 torch.multiprocessing.set_start_method('spawn')
 output_path = "/shares/perception-temp/voxceleb2/fecnet/train/"
-model = FECNet('FECNet.pt')
-mtcnn = MTCNN(image_size=224)
 
 def extractFecNetMultiVid(files):
     combine_faces = []
@@ -58,29 +56,31 @@ def extractFecNetMultiVid(files):
 
 def extractFecNet(files, buff):
     random.shuffle(files)
+    model = FECNet('FECNet.pt')
+    mtcnn = MTCNN(image_size=224)
     for file in files:
-        # try:
-        file_path_split = file.split("/")
-        id1, id2, fname = file_path_split[-3], file_path_split[-2], file_path_split[-1]
-        output_file_name = id1 + '_' + id2 + '_' + fname.split('.')[0] + '.csv'
-        output_file_path = os.path.join(output_path, output_file_name)
-        
-        if(os.path.isfile(output_file_path)):
+        try:
+            file_path_split = file.split("/")
+            id1, id2, fname = file_path_split[-3], file_path_split[-2], file_path_split[-1]
+            output_file_name = id1 + '_' + id2 + '_' + fname.split('.')[0] + '.csv'
+            output_file_path = os.path.join(output_path, output_file_name)
+            
+            if(os.path.isfile(output_file_path)):
+                continue
+            
+            vidcap = VideoFileClip(file)
+            frames = list(vidcap.iter_frames(fps=5))
+            
+            faces, prob = mtcnn(frames, return_prob=True)
+            faces = [t.numpy() for t in faces]
+            faces = np.array(faces)
+            if faces.any():
+                faces = torch.Tensor(faces).view(-1,3,224,224)
+                emb = model(faces.cuda()).cpu()
+                emb = emb.detach().numpy()
+                pd.DataFrame(emb).to_csv(output_file_path, header=None, index=False)
+        except:
             continue
-        
-        vidcap = VideoFileClip(file)
-        frames = list(vidcap.iter_frames(fps=5))
-        
-        faces, prob = mtcnn(frames, return_prob=True)
-        faces = [t.numpy() for t in faces]
-        faces = np.array(faces)
-        if faces.any():
-            faces = torch.Tensor(faces).view(-1,3,224,224)
-            emb = model(faces.cuda()).cpu()
-            emb = emb.detach().numpy()
-            pd.DataFrame(emb).to_csv(output_file_path, header=None, index=False)
-        # except:
-        #     continue
             
 def fecnet_extract_in_parallel(concurreny_count, files, fn):
     Processes = []
@@ -97,7 +97,7 @@ def fecnet_extract_in_parallel(concurreny_count, files, fn):
     for t in Processes:    t.join()
     
 target_chunk_size = 50
-concurreny_count = 5
+concurreny_count = 10
 meta_file_path = sys.argv[1]
 files = pd.read_csv(meta_file_path, header=None).values[:,0]
 
