b545a87dbcf955a02ba1f5e4c7a31a557fd91a30
nhatminh886688 <mtran14@u.rochester.edu>
2021-07-21 17:04:56 -0400

    fun stuff

--- extract_features.py
+++ extract_features.py
@@ -15,6 +15,7 @@ import random
 
 output_path = "/data/perception-temp/voxceleb2/fecnet/train/"
 
+
 def extractFecNet(files):
     random.shuffle(files)
     model = FECNet('FECNet.pt')
@@ -43,38 +44,34 @@ def extractFecNet(files):
         except:
             continue
             
-def extractFecNetSingle(file, model, mtcnn):
+def extractFecNetSingle(file):
     # try:
-    # model = FECNet('FECNet.pt')
-    # mtcnn = MTCNN(image_size=224)
-    file_path_split = file.split("/")
-    id1, id2, fname = file_path_split[-3], file_path_split[-2], file_path_split[-1]
-    output_file_name = id1 + '_' + id2 + '_' + fname.split('.')[0] + '.csv'
-    output_file_path = os.path.join(output_path, output_file_name)
-
-    if(os.path.isfile(output_file_path)):
-        return
-
-    vidcap = VideoFileClip(file)
-    frames = list(vidcap.iter_frames(fps=5))
-
-    faces, prob = mtcnn(frames, return_prob=True)
-    faces = [t.numpy() for t in faces]
-    faces = np.array(faces)
-    if faces.any():
-        faces = torch.Tensor(faces).view(-1,3,224,224)
-        emb = model(faces)
-        emb = emb.detach().numpy()
-        pd.DataFrame(emb).to_csv(output_file_path, header=None, index=False)
-        print(output_file_path)
-    # except:
-    #     return
-    
-def extractFecNetNNN(files):
     model = FECNet('FECNet.pt')
     mtcnn = MTCNN(image_size=224)
-    for file in files:
-        extractFecNetSingle(file, model, mtcnn)
+    try:
+        file_path_split = file.split("/")
+        id1, id2, fname = file_path_split[-3], file_path_split[-2], file_path_split[-1]
+        output_file_name = id1 + '_' + id2 + '_' + fname.split('.')[0] + '.csv'
+        output_file_path = os.path.join(output_path, output_file_name)
+
+        if(os.path.isfile(output_file_path)):
+            return
+
+        vidcap = VideoFileClip(file)
+        frames = list(vidcap.iter_frames(fps=5))
+
+        faces, prob = mtcnn(frames, return_prob=True)
+        faces = [t.numpy() for t in faces]
+        faces = np.array(faces)
+        if faces.any():
+            faces = torch.Tensor(faces).view(-1,3,224,224)
+            emb = model(faces)
+            emb = emb.detach().numpy()
+            pd.DataFrame(emb).to_csv(output_file_path, header=None, index=False)
+            print(output_file_path)
+    except:
+        return
+
             
 def fecnet_extract_in_parallel(concurreny_count, files, fn):
     Processes = []
@@ -103,11 +100,6 @@ files = pd.read_csv(meta_file_path, header=None).values[:,0]
 # random.shuffle(files_)
 # for files in files_:
 #     extractFecNetMultiVid(files)
-
-model = FECNet('FECNet.pt')
-mtcnn = MTCNN(image_size=224)
-
 pool = multiprocessing.Pool(4)
-files_ = np.array_split(files, concurreny_count)
-random.shuffle(files_)
-results =  [pool.apply(extractFecNetNNN, args = (files, )) for files in files_]
+pool.map(extractFecNetSingle, files)
+pool.close()
