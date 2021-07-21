c9d32bea8dde6a590e4dcbcc0f980bf1aaf0be80
nhatminh886688 <mtran14@u.rochester.edu>
2021-07-20 20:18:40 -0400

    asd

--- extract_features.py
+++ extract_features.py
@@ -11,6 +11,47 @@ from multiprocessing import Process, Manager
 import random
 
 output_path = "/shares/perception-temp/voxceleb2/fecnet/train/"
+model = FECNet('FECNet.pt')
+mtcnn = MTCNN(image_size=224)
+
+def extractFecNetMultiVid(files):
+    combine_faces = []
+    chunk_sizes = []
+    idx = 0
+    output_file_names = []
+    random.shuffle(files)
+    for file in files:
+        try:
+            file_path_split = file.split("/")
+            id1, id2, fname = file_path_split[-3], file_path_split[-2], file_path_split[-1]
+            output_file_name = id1 + '_' + id2 + '_' + fname.split('.')[0] + '.csv'
+            output_file_path = os.path.join(output_path, output_file_name)
+            
+            if(os.path.isfile(output_file_path)):
+                continue
+            
+            vidcap = VideoFileClip(file)
+            frames = list(vidcap.iter_frames(fps=5))
+            
+            faces, prob = mtcnn(frames, return_prob=True)
+            faces = [t.numpy() for t in faces]
+            faces = np.array(faces) # n_frames*3*224*224
+            
+            combine_faces.append(faces)
+            idx = idx + faces.shape[0]
+            chunk_sizes.append(idx)
+            output_file_names.append(output_file_path)
+        except:
+            continue
+    all_faces = np.concatenate(combine_faces, axis=0)
+    all_faces = torch.Tensor(all_faces).view(-1,3,224,224)
+    emb = model(all_faces.cuda()).cpu()
+    emb = emb.detach().numpy()
+    emb_split = np.split(emb, chunk_sizes)
+    for i in range(len(output_file_names)):
+        dout, fout = emb_split[i], output_file_names[i]
+        pd.DataFrame(dout).to_csv(fout, header=None, index=False)_
+    
 
 def extractFecNet(files, buff):
     model = FECNet('FECNet.pt')
@@ -53,7 +94,16 @@ def fecnet_extract_in_parallel(concurreny_count, files, fn):
     # block until all the threads finish (i.e. block until all function_x calls finish)
     for t in Processes:    t.join()
     
-concurreny_count = 15
+target_chunk_size = 50
+
 meta_file_path = sys.argv[1]
 files = pd.read_csv(meta_file_path, header=None).values[:,0]
-fecnet_extract_in_parallel(concurreny_count, files, extractFecNet)
+# fecnet_extract_in_parallel(concurreny_count, files, extractFecNet)
+concurreny_count = len(files) // target_chunk_size
+
+files_  =  [files[(i* (len(files)//concurreny_count)):((i+1)* (len(files)//concurreny_count))]    for i in range(concurreny_count)]
+leftovers  =  files[(concurreny_count * (len(files)//concurreny_count))  :  len(files)]
+for i in range(len(leftovers)):    files_[i] += [leftovers[i]]
+
+for files in files_:
+    extractFecNetMultiVid(files)
