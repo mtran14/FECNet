c4f5ab182c66f46090d37107c9ecf3aef3d95b03
Minh Tran <mtran@smtp.ict.usc.edu>
2021-07-21 18:29:54 -0700

    fun stuff

--- meta.sh
+++ meta.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in {0..15}
+for i in {0..14}
 do
 	bash mscript.sh $i &
 done
--- mscript.sh
+++ mscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true
 do
-	timeout 600 taskset --cpu-list $1 python extract_features.py
+	timeout 60 taskset --cpu-list $1 python extract_features.py
 done
 
